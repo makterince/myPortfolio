6aa25d917fdc815826449422c7b9ec7d9d89cb83
Festus Omaku <fomaku3@gmail.com>
2023-10-29 15:37:02 +0100

    portfolio website

---
+++ README.md
@@ -0,0 +1,1 @@
+this is my portfolio webstite. still undergoing improvements
---
+++ script.js
@@ -0,0 +1,27 @@
+// Dark mode toggle
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
+
+//tech stack section
+const techItems = document.querySelectorAll('.tech-item');
+
+techItems.forEach(item => {
+    const proficiencyBar = item.querySelector('.proficiency-bar');
+    const proficiencyLevel = proficiencyBar.style.width;
+    proficiencyBar.style.width = proficiencyLevel;
+});
+
+//thumbs up animation
+const smiley = document.getElementById('smiley');
+smiley.addEventListener('mouseenter', () => {
+    smiley.style.transform = 'scale(1.2) rotate(-15deg)';
+});
+
+smiley.addEventListener('mouseleave', () => {
+    smiley.style.transform = 'scale(1)';
+});
+
